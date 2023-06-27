1e17f89b781809583d8dc4c7c20667df50dca0a9
KJun <dkdlel10111@naver.com>
2023-06-27 20:29:15 +0900

    test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -21,7 +21,7 @@ import project_java.Planner_InsertSpot;
 import project_java.Planner_Select;
 import project_java.Pw_Search;
 
-// 테스트23123
+// 테스트2312333333333
 public class Main extends JFrame{
 	public CardLayout cardLayout;
 	public JPanel cardJPanel;
