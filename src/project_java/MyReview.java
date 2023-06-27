01985290ae51353e21a8a292badf337a2bf249c2
Lily <rlaalswl8077@gmail.com>
2023-06-27 12:25:38 +0900

    ID/PW 찾기, 마이페이지, 리뷰페이지 + 로그인메인 수정_230627

--- src/project_java/MyReview.java
+++ src/project_java/MyReview.java
@@ -2,6 +2,7 @@ package project_java;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -14,21 +15,23 @@ import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultCellEditor;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class MyReview extends JPanel{
 	JPanel jp, jp_headerMain, jp_headerSub, jp_headerSubLeft, jp_headerSubRight, jp_buttons, jp_east, jp_west, jp_south, 
@@ -37,32 +40,60 @@ public class MyReview extends JPanel{
 	Font customFont;
 	JLabel jLabel1;
 	
-	JPanel lb_jp, bt1_jp, bt2_jp, review1_jp, review2_jp, wr_jp, viewlb_jp, combo_jp, view_jp, jtf1_jp, jtf2_jp,add1_jp, add2_jp, jp_center;
+	JPanel lb_jp, bt1_jp, bt2_jp, review1_jp, review2_jp, wr_jp, viewlb_jp, combo_jp, view_jp, jtf1_jp, jtf2_jp,
+	  viewjp, writejp, add1_jp, add2_jp, jp_center;
 	JLabel re_lb, wr_lb, view_lb;
 	JTextField review_jtf;
-	JTextArea review_jta;
+	//JTextArea review_jta;
 	JScrollPane review_jsp;
 	JButton re1_bt, re2_bt, re3_bt, re4_bt;
 	JComboBox<String> serch1, serch2;
-	
+	//GridBagConstraints gbc;
 	Border newBorder;
+	Object [] colNames = {"Check","글번호","제목","내용"};
+	Object [][] datas = {{false,"1","가평캠핑","불멍 물멍 힐링했어요루룰"},{true,"2","일본 탐방","오사카 난바 더웠지만 재밌어요"}};
+	JTable table;
+	JCheckBox box;
+	DefaultTableModel dtm;
 	Main main;
 	
 	public MyReview(Main main) {
 
-		this.main = main;
+	this.main = main;
 //	FONT
 //	Font font = Font.loadFont("src/homework/fonts/Jalnan.ttf");
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		try {
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/Jalnan.ttf")));
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts//Doodly.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/team/login/logo_e/Jalnan.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/team/login/logo_e/Doodly.ttf")));
 		} catch (FontFormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		//콤보박스 메서드
+		DefaultTableCellRenderer dcr = new DefaultTableCellRenderer(){
+			public Component getTableCellRendererComponent  // 셀렌더러
+			(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+				JCheckBox box= new JCheckBox();
+				box.setSelected(((Boolean)value).booleanValue());  
+				box.setHorizontalAlignment(JLabel.CENTER);
+				return box;
+			}
+		};
+		/*
+		{//test구간
+			jp.setLayout(new GridBagLayout());
 
+			// wr_lb를 jp의 가운데에 정렬하기 위한 GridBagConstraints 설정
+			gbc = new GridBagConstraints();
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			gbc.fill = GridBagConstraints.CENTER;
+			
+		}*/
+		
 //	변수 생성
 		{
 			jp = new JPanel();
@@ -100,7 +131,7 @@ public class MyReview extends JPanel{
 			
 			jp_center.setLayout(new BoxLayout(jp_center, BoxLayout.Y_AXIS));
 			//jp_center.setLayout(new GridLayout(4,2));
-			jp_center.setBorder(new EmptyBorder(50, 100, 30, 100));
+			jp_center.setBorder(new EmptyBorder(50, 100, 50, 100));
 			//JPanel jp_center2 = new JPanel();
 			//jp.add(jp_center2, BorderLayout.CENTER);
 			//jp_center2.setLayout(new BorderLayout());
@@ -169,7 +200,7 @@ public class MyReview extends JPanel{
 		
 
 //	add image 
-		ImageIcon imageIcon = new ImageIcon("src/images/only_logo.png");
+		ImageIcon imageIcon = new ImageIcon("src/team/login/logo_e/logo3.png");
 		Image image = imageIcon.getImage(); // transform it
 		Image newimg = image.getScaledInstance(70, 70, java.awt.Image.SCALE_DEFAULT); // scale it the smooth way
 		imageIcon = new ImageIcon(newimg); // transform it back
@@ -211,13 +242,13 @@ public class MyReview extends JPanel{
 			review2_jp = new JPanel();
 			review2_jp.setOpaque(false);
 			review_jtf = new JTextField(50);
-			review_jta = new JTextArea();
+			/*review_jta = new JTextArea();
 			review_jsp = new JScrollPane(review_jta, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 								ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 			review_jta.setLineWrap(true);
-			review_jta.setEditable(false);
+			review_jta.setEditable(false);*/
 			re1_bt = new JButton("전체 후기");
-			re2_bt = new JButton("내 후기");
+			re2_bt = new JButton("후기 작성");
 			re3_bt = new JButton("작성");
 			re4_bt = new JButton("삭제");
 			combo_jp = new JPanel();
@@ -235,14 +266,10 @@ public class MyReview extends JPanel{
 			re_lb.setFont(new Font("Jalnan",Font.PLAIN,20));
 			wr_lb.setFont(new Font("Jalnan",Font.PLAIN,20));
 			view_lb.setFont(new Font("Jalnan",Font.PLAIN,20));
-			re1_bt.setFont(new Font("Jalnan",Font.PLAIN,10));
-			re2_bt.setFont(new Font("Jalnan",Font.PLAIN,10));
-			re3_bt.setFont(new Font("Jalnan",Font.PLAIN,15));
-			re4_bt.setFont(new Font("Jalnan",Font.PLAIN,15));
-			re3_bt.setForeground(Color.WHITE);
-			re4_bt.setForeground(Color.WHITE);
-			re3_bt.setPreferredSize(new Dimension(100,30));
-			re4_bt.setPreferredSize(new Dimension(100,30));
+			re1_bt.setFont(new Font("Jalnan",Font.PLAIN,16));
+			re2_bt.setFont(new Font("Jalnan",Font.PLAIN,16));
+			re3_bt.setFont(new Font("Jalnan",Font.PLAIN,12));
+			re4_bt.setFont(new Font("Jalnan",Font.PLAIN,12));
 			re1_bt.setOpaque(false);
 			re1_bt.setContentAreaFilled(false);
 			re2_bt.setOpaque(false);
@@ -251,7 +278,7 @@ public class MyReview extends JPanel{
 			re4_bt.setBackground(Color.decode("#D4B8E8"));
 			
 			// 패널에 붙이기
-			//라벨
+			//1. 왼쪽 라벨
 			lb_jp.add(re_lb);
 			// 위치조정
 			lb_jp.setBorder(BorderFactory.createEmptyBorder(15,0,0,0));
@@ -269,8 +296,8 @@ public class MyReview extends JPanel{
 			combo_jp.add(serch2);
 			
 			
-			// 리뷰 작성, 보기 패널 붙이기
-			wr_jp.add(wr_lb);
+			// 2. 리뷰 작성, 보기 패널 붙이기
+			//wr_jp.add(wr_lb);
 			// 위치조정
 			wr_lb.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
 			review1_jp.add(serch1);
@@ -290,16 +317,32 @@ public class MyReview extends JPanel{
 			
 			
 			//모든 panel들 합칠 panel의 레이아웃 box로 설정
-			add2_jp.setLayout(new BoxLayout(add2_jp, BoxLayout.Y_AXIS));
-			view_jp.add(view_lb);
-			view_lb.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
-			view_jp.add(review_jsp);
-			//viewlb_jp.setBorder(new EmptyBorder(50, 50, 50, 50));
+			viewjp = new JPanel();
+			viewjp.setLayout(new FlowLayout());
+			viewjp.add(view_lb);
+			viewjp.setBackground(Color.WHITE);
+			view_jp.setLayout(new BorderLayout());
+			view_jp.add(viewjp, BorderLayout.NORTH);
+			//view_lb.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
+			//view_jp.add(review_jsp);
+			//viewlb_jp.setBorder(new EmptyBorder(0, 500, 0, 0));
 			//크기조정
-			review_jsp.setPreferredSize(new Dimension(600,100));
-			view_jp.add(re4_bt);
+			//review_jsp.setPreferredSize(new Dimension(600,100));
+			Object [] colNames = {"Check","글번호","제목","내용"};
+			Object [][] datas = {{false,"1","가평캠핑","불멍 물멍 힐링했어요루룰"},{true,"2","일본 탐방","오사카 난바 더웠지만 재밌어요"}};
+			dtm = new DefaultTableModel(datas, colNames);
+			table = new JTable(dtm);
+			table.getColumn("Check").setCellRenderer(dcr);
+			box = new JCheckBox();
+			box.setHorizontalAlignment(JLabel.CENTER);
+			table.getColumn("Check").setCellEditor(new DefaultCellEditor(box));
+			review_jsp = new JScrollPane(table);
+			review_jsp.setPreferredSize(new Dimension(500,220));
+			//add2_jp.setLayout(new BoxLayout(add2_jp, BoxLayout.Y_AXIS));
+			view_jp.add(review_jsp, BorderLayout.CENTER);
+			view_jp.add(re4_bt, BorderLayout.SOUTH);
 			
-			add1_jp.add(wr_lb);
+			add1_jp.add(wr_lb, BorderLayout.CENTER);
 			add1_jp.add(combo_jp);
 			add1_jp.add(review1_jp);
 			add2_jp.add(viewlb_jp);
@@ -336,7 +379,7 @@ public class MyReview extends JPanel{
 			
 			setLayout(new BorderLayout());
 			add(jp_headerMain, BorderLayout.NORTH);
-			add(jp, BorderLayout.CENTER);
+			add(jp, BorderLayout.CENTER);			
 	  }
 	}
 	public MyReview() {
@@ -347,7 +390,6 @@ public class MyReview extends JPanel{
 		try {
 			// Select the Look and Feel
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-			UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
 
 			SwingUtilities.invokeLater(new Runnable() {
 
