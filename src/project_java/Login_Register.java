de398fb3d7ee19c30e117ee9f12d8d0e92789e14
Lily <rlaalswl8077@gmail.com>
2023-06-28 20:35:54 +0900

    회원가입 버튼 rev.0

--- src/project_java/Login_Register.java
+++ src/project_java/Login_Register.java
@@ -182,14 +182,14 @@ public class Login_Register extends JPanel implements ActionListener {
 			pwcheck_jp.add(new JLabel(pwIcon));
 			pwcheck_jp.add(tf_pw2);
 
-			// 메일주소 / 아이콘 + 텍스트필드
+			// 메일주소(NN) / 아이콘 + 텍스트필드
 			ImageIcon mailIcon = new ImageIcon("src/icons/mail.png");
 			Image mailImage = mailIcon.getImage();
 			Image newmailImg = mailImage.getScaledInstance(20, 20, java.awt.Image.SCALE_DEFAULT);
 			mailIcon = new ImageIcon(newmailImg);
 			tf_mail = new JTextField(23);
 			tf_mail.setPreferredSize(new Dimension(50, 30));
-			TextPrompt tp_mail = new TextPrompt("비밀번호 분실 시 확인용 이메일[선택] ", tf_mail);
+			TextPrompt tp_mail = new TextPrompt("비밀번호 분실 시 확인용 이메일", tf_mail);
 			tp_mail.setFont(new Font("Jalnan", Font.PLAIN, 10));
 			tp_mail.setForeground(Color.gray);
 			tp_mail.changeAlpha(0.5f);
@@ -258,7 +258,40 @@ public class Login_Register extends JPanel implements ActionListener {
 			ta_TermsofUse.setFont(new Font("Jalnan", Font.PLAIN, 10));
 			ta_TermsofUse.setBackground(Color.WHITE);
 			ta_TermsofUse.setForeground(Color.BLACK);
-			ta_TermsofUse.setText("개인정보 약관\n약관내용~~~~~~~");
+			ta_TermsofUse.setText("개인정보 약관\n개인정보보호법에 따라 퍼스널플래너에 회원가입 신청하시는 분께 수집하는 개인정보의 항목, 개인정보의 수집 및 이용목적, 개인정보의 보유 및 이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내 드리오니 자세히 읽은 후 동의하여 주시기 바랍니다.\r\n"
+					+ "\r\n"
+					+ "1. 수집하는 개인정보\r\n"
+					+ "이용자가 여행일정 만들기 등 개인화 혹은 회원제 서비스를 이용하기 위해 회원가입을 할 경우, 퍼스널플래너는 서비스 이용을 위해 필요한 최소한의 개인정보를 수집합니다.\r\n"
+					+ "\r\n"
+					+ "회원가입 시점에 퍼스널플래너가 이용자로부터 수집하는 개인정보는 아래와 같습니다.\r\n"
+					+ "- 회원 가입 시 필수항목으로 아이디, 비밀번호, 이름, 생년월일, 휴대전화번호, 본인확인 이메일주소를 수집합니다. 동일인 식별정보(CI), 중복가입 확인정보(DI) 함께 수집합니다.\r\n"
+					+ "서비스 이용 과정에서 이용자로부터 수집하는 개인정보는 아래와 같습니다.\r\n"
+					+ "- 회원정보 또는 개별 서비스에서 프로필 정보를 설정할 수 있습니다.\r\n"
+					+ "- 퍼스널플래너 내의 개별 서비스 이용시 이용자에 한해 추가 개인정보 수집이 발생할 수 있습니다. 추가로 개인정보를 수집할 경우에는 해당 개인정보 수집 시점에서 이용자에게 ‘수집하는 개인정보 항목, 개인정보의 수집 및 이용목적, 개인정보의 보관기간’에 대해 안내 드리고 동의를 받습니다.\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "서비스 이용 과정에서 서비스 이용 기록이 생성되어 수집될 수 있습니다.\r\n"
+					+ "구체적으로 1) 서비스 이용 과정에서 이용자에 관한 정보를 자동화된 방법으로 생성하여 이를 저장(수집)하거나, 2) 이용자 기기의 고유한 정보를 원래의 값을 확인하지 못 하도록 안전하게 변환하여 수집합니다.\r\n"
+					+ "이와 같이 수집된 정보는 개인정보와의 연계 여부 등에 따라 개인정보에 해당할 수 있고, 개인정보에 해당하지 않을 수도 있습니다.\r\n"
+					+ "2. 수집한 개인정보의 이용\r\n"
+					+ "퍼스널플래너의 회원관리, 서비스 개발・제공 및 향상, 안전한 이용환경 구축 등 아래의 목적으로만 개인정보를 이용합니다.\r\n"
+					+ "\r\n"
+					+ "- 회원 가입 의사의 확인, 연령 확인 및 이용자의 본인 확인, 이용자 식별, 회원탈퇴 의사의 확인 등 회원관리를 위하여 개인정보를 이용합니다.\r\n"
+					+ "- 법령 및 퍼스널플래너 이용약관을 위반하는 회원에 대한 이용 제한 조치, 부정 이용 행위를 포함하여 서비스의 원활한 운영에 지장을 주는 행위에 대한 방지 및 제재, 계정도용 및 부정거래 방지, 약관 개정 등의 고지사항 전달, 분쟁조정을 위한 기록 보존, 민원처리 등 이용자 보호 및 서비스 운영을 위하여 개인정보를 이용합니다.\r\n"
+					+ "- 보안, 프라이버시, 안전 측면에서 이용자가 안심하고 이용할 수 있는 서비스 이용환경 구축을 위해 개인정보를 이용합니다.\r\n"
+					+ "3. 개인정보의 보관기간\r\n"
+					+ "퍼스널플래너는 원칙적으로 이용자의 개인정보를 회원 탈퇴 7일 후 파기하고 있습니다.\r\n"
+					+ "단, 이용자에게 개인정보 보관기간에 대해 별도의 동의를 얻은 경우, 또는 법령에서 일정 기간 정보보관 의무를 부과하는 경우에는 해당 기간 동안 개인정보를 안전하게 보관합니다.\r\n"
+					+ "이용자에게 개인정보 보관기간에 대해 회원가입 시 또는 서비스 가입 시 동의를 얻은 경우는 아래와 같습니다.\r\n"
+					+ "- 부정 가입 및 이용 방지\r\n"
+					+ "부정 이용자의 정보 : 탈퇴일로부터 7일 보관\r\n"
+					+ "탈퇴한 이용자의 휴대전화번호(복호화가 불가능한 일방향 암호화(해시처리)) : 탈퇴일로부터 7일 보관\r\n"
+					+ "전자상거래 등에서의 소비자 보호에 관한 법률, 전자문서 및 전자거래 기본법, 통신비밀보호법 등 법령에서 일정기간 정보의 보관을 규정하는 경우는 아래와 같습니다. 네이버는 이 기간 동안 법령의 규정에 따라 개인정보를 보관하며, 본 정보를 다른 목적으로는 절대 이용하지 않습니다.\r\n"
+					+ "- 전자상거래 등에서 소비자 보호에 관한 법률\r\n"
+					+ "\r\n"
+					+ "4. 개인정보 수집 및 이용 동의를 거부할 권리\r\n"
+					+ "이용자는 개인정보의 수집 및 이용 동의를 거부할 권리가 있습니다. 회원가입 시 수집하는 최소한의 개인정보, 즉, 필수 항목에 대한 수집 및 이용 동의를 거부하실 경우, 회원가입이 어려울 수 있습니다.");
 
 //			약관 체크박스(NN)
 			cb_TermsofUse = new JCheckBox("개인정보조회 및 수집, 이용, 제공 동의");
@@ -326,13 +359,40 @@ public class Login_Register extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		JButton obj = (JButton) e.getSource();
 		if (obj == join_bt) { // 가입하기 버튼
-			int ok = 0;
-			if(ok ==1) { // 정상적으로 입력시
+			// 누락된 정보 있을시
+			if(tf_id.getText().equals("")) { // 아이디 공란일경우
+				JOptionPane.showMessageDialog(null, "아이디를 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_id.requestFocus();// 커서위치 조절 
+			} else if(tf_pw.getText().equals("")) { // 비밀번호 공란일경우
+				JOptionPane.showMessageDialog(null, "비밀번호를 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_pw.requestFocus();
+			} else if(tf_pw2.getText().equals("")) { // 비밀번호 확인란 공란일경우
+				JOptionPane.showMessageDialog(null, "비밀번호를 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_pw2.requestFocus();
+			} else if(!tf_pw.getText().equals(tf_pw2.getText())) { // 입력한비밀번호가 다를경우
+				JOptionPane.showMessageDialog(null, "비밀번호가 일치하지 않습니다!", "Confirm", JOptionPane.ERROR_MESSAGE);
+			} else if(tf_mail.getText().equals("")) { // 이메일 공란일경우
+				JOptionPane.showMessageDialog(null, "이메일을 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_mail.requestFocus(); 
+			} else if(tf_name.getText().equals("")) { // 이름 공란일경우
+				JOptionPane.showMessageDialog(null, "이름을 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_name.requestFocus(); 
+			} else if(tf_birth.getText().equals("")) { // 생년월일 공란일경우
+				JOptionPane.showMessageDialog(null, "생년월일을 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_birth.requestFocus(); 
+			} else if(tf_phone.getText().equals("")) { // 휴대전화 공란일경우
+				JOptionPane.showMessageDialog(null, "휴대전화 번호를 입력해주세요!", "Confirm", JOptionPane.ERROR_MESSAGE);
+				tf_phone.requestFocus(); 
+			} else {
 				JOptionPane.showMessageDialog(null, "회원가입이 완료되었습니다!", " Confirm", JOptionPane.INFORMATION_MESSAGE);
+				tf_id.setText("");
+				tf_pw.setText("");
+				tf_pw2.setText("");
+				tf_mail.setText("");
+				tf_name.setText("");
+				tf_birth.setText("");
+				tf_phone.setText("");
 				main.cardLayout.show(main.cardJPanel, "login_Main");
-			} else if(ok==0) { // 누락된 정보 있을시
-
-				JOptionPane.showMessageDialog(null, "아이디를 입력해주세요!", "Confirm",JOptionPane.ERROR_MESSAGE);
 			}
 		} else if (obj == cancel_bt) {
 			main.cardLayout.show(main.cardJPanel, "login_Main");
