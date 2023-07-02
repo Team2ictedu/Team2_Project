c3162f38f03b464eabec73c29fb0598735f4b520
KJun <dkdlel10111@naver.com>
2023-07-02 19:39:58 +0900

    회원가입 합치기 완

--- src/User/UserDAO.java
+++ src/User/UserDAO.java
@@ -67,6 +67,33 @@ public class UserDAO {
 		}
 		return null;
 	}	
+	// 삽입
+	public int getUserRegister(UserVO vo) {
+		try {
+			conn = getConnection();
+			String sql = "insert into MEMBER(M_ID, M_PW, M_EMAIL, M_NAME, M_BIRTH, M_PHONE, M_TERMS, M_CLASS) values(?,?,?,?,?,?,?,?)";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, vo.getM_ID());
+			pstm.setString(2, vo.getM_PW());
+			pstm.setString(3, vo.getM_EMAIL());
+			pstm.setString(4, vo.getM_NAME());
+			pstm.setString(5, vo.getM_BIRTH());
+			pstm.setString(6, vo.getM_PHONE());
+			pstm.setString(7, vo.getM_TERMS());
+			pstm.setString(8, "1");
+			int result = pstm.executeUpdate();
+			return result;
+		} catch (Exception e) {
+			System.out.print(e);
+		} finally {
+			try {
+				pstm.close();
+				conn.close();
+			} catch (Exception e2) {
+			}
+		}
+		return 0;
+	}
 	
 	// 개인정보 변경
 	public int getUserUpdate(UserVO vo){
